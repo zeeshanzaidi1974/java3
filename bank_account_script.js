13d6501f03a3acd31c736c355d600841e73b87b9
zeeshanzaidi1974 <98213611+zeeshanzaidi1974@users.noreply.github.com>
2022-06-13 17:11:40 +0200

    Update bank_account_script.js

--- bank_account_script.js
+++ bank_account_script.js
@@ -31,7 +31,7 @@ btnDeposit.addEventListener('click', () => {
         document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
 
   console.log("$" + userDeposit.value);
-    return userDeposit.value = '';
+    
     }
 }
     
